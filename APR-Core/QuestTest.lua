404069a727706f7e3a914a2bf5a1977e3e1fab70
Aldric Ducreux <neogeekmo@live.fr>
2022-11-16 01:18:55 +0100

    fix typo TRAIN_RIDING

--- APR-Core/QuestTest.lua
+++ APR-Core/QuestTest.lua
@@ -699,10 +699,10 @@ function APR.UpdateZoneQuestOrderList(APRmod)
 					end
 				end
 				if (APR.QuestStepList[APR.ActiveMap][CCLi]["DalaranToOgri"]) then
-					APR.ZoneQuestOrder["FS2"][CLi]:SetText(L["USE_ORGRIMMAR_PORTAL"])
+					APR.ZoneQuestOrder["FS2"]:SetText(L["USE_ORGRIMMAR_PORTAL"])
 				end
 				if (APR.QuestStepList[APR.ActiveMap][CCLi]["PahonixMadeMe"]) then
-					APR.ZoneQuestOrder["FS2"][CLi]:SetText("TRAIN_RIDING")
+					APR.ZoneQuestOrder["FS2"][CLi]:SetText(L["TRAIN_RIDING"])
 				end
 				if (APR.QuestStepList[APR.ActiveMap][CCLi]["UseGarrisonHS"]) then
 					APR.ZoneQuestOrder["FS2"][CLi]:SetText(L["USE_GARRISON_HEARTHSTONE"])
