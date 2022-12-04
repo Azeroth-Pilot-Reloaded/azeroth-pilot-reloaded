7e7b1991967534f48532ea16448d4eaef9d3f5bd
Aldric Ducreux <neogeekmo@live.fr>
2022-12-04 19:59:44 +0100

    duplicate parameter

--- APR-Core/locales/frFR.lua
+++ APR-Core/locales/frFR.lua
@@ -3,4 +3,4 @@ if not L then return end
 
 -- Author: Neogeekmo
 --------------------------------------------------------------------------------------------------------------------------------------------
-@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore", handle-unlocalized="ignore")@
+@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore")@
