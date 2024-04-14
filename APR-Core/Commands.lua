3e75b375d6240b0f7218764cc520729fc86872ea
Aldric Ducreux <neogeekmo@live.fr>
2024-04-14 18:31:37 +0200

    Add a to lowercase for command

--- APR-Core/Commands.lua
+++ APR-Core/Commands.lua
@@ -4,46 +4,47 @@ local L = LibStub("AceLocale-3.0"):GetLocale("APR")
 APR.command = APR:NewModule("Command")
 -- Chat commands, such as /apr reset, /apr skip, /apr skipcamp
 function APR.command:SlashCmd(input)
+    local inputText = string.lower(input)
     if not APR.settings.profile.enableAddon then
         APR.settings:OpenSettings(APR.title)
     end
-    if (input == "reset" or input == "r") then
+    if (inputText == "reset" or inputText == "r") then
         --Command for making the quest rescan on completion and reset, including previously skipped steps
         print("APR: " .. L["RESET_ZONE"])
         APRData[APR.PlayerID][APR.ActiveRoute] = 1
         C_UI.Reload()
-    elseif (input == "forcereset" or input == "fr") then
+    elseif (inputText == "forcereset" or inputText == "fr") then
         -- APRData[APR.PlayerID].FirstLoad = true
         APRData[APR.PlayerID] = {}
         APRZoneCompleted[APR.PlayerID] = nil
         APRCustomPath[APR.PlayerID] = {}
         C_UI.Reload()
-    elseif (input == "skip" or input == "s" or input == "skippiedoodaa") then
+    elseif (inputText == "skip" or inputText == "s" or inputText == "skippiedoodaa") then
         -- Command for skipping the current quest step
         print("APR: " .. L["SKIP"])
         NextQuestStep()
-    elseif (input == "rollback" or input == "rb") then
+    elseif (inputText == "rollback" or inputText == "rb") then
         -- Command for rollback the current quest step
         print("APR: " .. L["ROLLBACK"])
         PreviousQuestStep()
-    elseif (input == "qol") then
+    elseif (inputText == "qol") then
         APR.settings.profile.showQuestOrderList = not APR.settings.profile.showQuestOrderList
         APR.questOrderList:RefreshFrameAnchor()
-    elseif (input == "discord") then
+    elseif (inputText == "discord") then
         _G.StaticPopup_Show("Discord_Link")
-    elseif (input == "github") then
+    elseif (inputText == "github") then
         _G.StaticPopup_Show("Github_Link")
-    elseif (input == "scribe" or input == "writer") then
+    elseif (inputText == "scribe" or inputText == "writer") then
         APR.questionDialog:CreateMessagePopup(L["SCRIBE_HEADER"] .. "\n\n" .. L["SCRIBE"], L["CLOSE"])
-    elseif input == 'coord' then
+    elseif inputText == 'coord' then
         APR.settings.profile.coordinateShow = not APR.settings.profile.coordinateShow
         APR.coordinate:RefreshFrameAnchor()
-    elseif input == 'route' then
+    elseif inputText == 'route' then
         APR.settings:OpenSettings(L["ROUTE"])
-    elseif input == '42' then
+    elseif inputText == '42' then
         PlaySoundFile("Interface\\Addons\\APR-Core\\assets\\42.mp3")
         UIErrorsFrame:AddMessage(L["42_COMMAND"], 1.0, 1.0, 0.0, 1.0, UIERRORS_HOLD_TIME)
-    elseif (input == "help" or input == "h") then
+    elseif (inputText == "help" or inputText == "h") then
         print(L["COMMAND_LIST"] .. ":")
         print("|cffeda55f/apr help, h |r- " .. L["HELP_COMMAND"])
         print("|cffeda55f/apr route |r- " .. L["ROUTE_COMMAND"])
