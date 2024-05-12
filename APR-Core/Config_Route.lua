855b5d3c4dc2853fd5a2768471020bfa6c7d3771
Aldric Ducreux <neogeekmo@live.fr>
2024-05-12 22:20:09 +0200

    Add Dread Wastes Horde route

--- APR-Core/Config_Route.lua
+++ APR-Core/Config_Route.lua
@@ -828,6 +828,7 @@ function APR.routeconfig:GetMoPRemixPrefab()
         tinsert(APRCustomPath[APR.PlayerID], "Krasarang Wilds")
         tinsert(APRCustomPath[APR.PlayerID], "Kun-Lai Summit")
         tinsert(APRCustomPath[APR.PlayerID], "Townlong Steppes")
+        tinsert(APRCustomPath[APR.PlayerID], "Dread Wastes")
     end
     self:SendMessage("APR_Custom_Path_Update")
 end
--- APR-Core/database/HordeRoutes.lua
+++ APR-Core/database/HordeRoutes.lua
@@ -23,6 +23,7 @@ if (APR.Faction == "Horde") then
         ["418-Krasarang Wilds"] = "Krasarang Wilds",
         ["379-Kun-Lai Summit"] = "Kun-Lai Summit",
         ["388-Townlong Steppes"] = "Townlong Steppes",
+        ["390-Dread Wastes"]  = "Dread Wastes",
     }
     APR.RouteList.WarlordsOfDraenor = {
         ["85-DesMephisto-Orgrimmar-p1"] = "WOD01 - Orgrimmar",
