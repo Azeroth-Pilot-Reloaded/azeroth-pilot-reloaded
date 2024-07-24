8b530e2b123dee64619f4ca1e709e06fa52081ca
Aldric Ducreux <neogeekmo@live.fr>
2024-07-24 17:11:27 +0200

    Udpate lib with prepatch

--- APR-Core/libs/HereBeDragons/HereBeDragons-2.0.lua
+++ APR-Core/libs/HereBeDragons/HereBeDragons-2.0.lua
@@ -19,6 +19,7 @@ local WoWClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 local WoWBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
 local WoWWrath = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
 local WoWCata = (WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC)
+local WoW11 = select(4, GetBuildInfo()) >= 110000
 
 -- Data Constants
 local COSMIC_MAP_ID = 946
@@ -125,7 +126,6 @@ if not oldversion or oldversion < 23 then
             { 1545, 1220, -2666.7, 0, 4800, 8000, 0, -0 },
             { 1599, 1, 4800, 5866.7, -4266.7, -3200, -490.6, -0.4 },
             { 1609, 571, 6400, 8533.3, -1600, 533.3, 512.8, 545.3 },
-            { 2601, 2552, -3881.25, 5085.42, -7487.5, 5962.5, -4640.6, -6861.9 },
         }
     end
 
@@ -155,7 +155,7 @@ if not oldversion or oldversion < 23 then
         return instanceID, left, right, top, bottom
     end
 
-    local vector00, vector05 = CreateVector2D(0, 0), CreateVector2D(0.5, 0.5)
+    local vector025, vector075 = CreateVector2D(0.25, 0.25), CreateVector2D(0.75, 0.75)
     -- gather the data of one map (by uiMapID)
     local function processMap(id, data, parent)
         if not id or mapData[id] then return end
@@ -171,14 +171,18 @@ if not oldversion or oldversion < 23 then
             parent = 0
         end
 
-        -- get two positions from the map, we use 0/0 and 0.5/0.5 to avoid issues on some maps where 1/1 is translated inaccurately
-        local instance, topLeft = C_Map.GetWorldPosFromMapPos(id, vector00)
-        local _, bottomRight = C_Map.GetWorldPosFromMapPos(id, vector05)
+        -- get two positions from the map, we use the center 0.5 square to avoid issues on some maps where 1/1 is translated inaccurately
+        local instance, topLeft = C_Map.GetWorldPosFromMapPos(id, vector025)
+        local _, bottomRight = C_Map.GetWorldPosFromMapPos(id, vector075)
         if topLeft and bottomRight then
             local top, left = topLeft:GetXY()
             local bottom, right = bottomRight:GetXY()
-            bottom = top + (bottom - top) * 2
-            right = left + (right - left) * 2
+            local height = (bottom - top) * 2
+            local width = (right - left) * 2
+            top = top - height / 4
+            bottom = top + height
+            left = left - width / 4
+            right = left + width
 
             instance, left, right, top, bottom = applyMapTransforms(instance, left, right, top, bottom)
             mapData[id] = {left - right, top - bottom, left, top, instance = instance, name = data.name, mapType = data.mapType, parent = parent }
@@ -240,6 +244,17 @@ if not oldversion or oldversion < 23 then
             worldMapData[0] = { 48033.24, 32020.8, 36867.97, 14848.84 }
             worldMapData[1] = { 47908.72, 31935.28, 8552.61, 18467.83 }
             worldMapData[571] = { 47662.7, 31772.19, 25198.53, 11072.07 }
+        elseif WoW11 then
+            worldMapData[0] = { 76153.14, 50748.62, 65008.24, 23827.51 }
+            worldMapData[1] = { 77621.12, 51854.98, 12444.4, 28030.61 }
+            worldMapData[571] = { 71773.64, 50054.05, 36205.94, 12366.81 }
+            worldMapData[870] = { 67710.54, 45118.08, 33565.89, 38020.67 }
+            worldMapData[1220] = { 82758.64, 55151.28, 52943.46, 24484.72 }
+            worldMapData[1642] = { 77933.3, 51988.91, 44262.36, 32835.1 }
+            worldMapData[1643] = { 76060.47, 50696.96, 55384.8, 25774.35 }
+            worldMapData[2444] = { 111420.37, 74283, 86088.21, 15682.4 }
+            worldMapData[2552] = { 82171.44, 54787.67, 21219.3, 47876.05 }
+            worldMapData[2601] = { 67929.29, 49267.42, 18325.63, 42233.06 }
         else
             worldMapData[0] = { 76153.14, 50748.62, 65008.24, 23827.51 }
             worldMapData[1] = { 77621.13, 51854.98, 18576.47, 28030.61 }
