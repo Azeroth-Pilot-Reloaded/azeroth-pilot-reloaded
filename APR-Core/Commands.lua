621f2ec70c7a526241aafc23594f5f4c03e951d7
Aldric Ducreux <neogeekmo@live.fr>
2024-08-07 03:21:24 +0200

    Add step 0 for the tauren

--- APR-Core/Commands.lua
+++ APR-Core/Commands.lua
@@ -26,10 +26,12 @@ function APR.command:SlashCmd(input)
         -- Command for skipping the current quest step
         print("APR: " .. L["SKIP"])
         APR:NextQuestStep()
+        APR.BookingList["UpdateMapId"] = true
     elseif (inputText == "rollback" or inputText == "rb") then
         -- Command for rollback the current quest step
         print("APR: " .. L["ROLLBACK"])
         APR:PreviousQuestStep()
+        APR.BookingList["UpdateMapId"] = true
     elseif (inputText == "qol") then
         APR.settings.profile.showQuestOrderList = not APR.settings.profile.showQuestOrderList
         APR.questOrderList:RefreshFrameAnchor()
