ff9478bd3d7848e11c845b7a3d941bff4e7cc813
Aldric Ducreux <neogeekmo@live.fr>
2025-03-21 01:15:46 +0100

    Fix emoji

--- .github/scripts/post_discord.py
+++ .github/scripts/post_discord.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import requests
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,6 +18,7 @@ if len(sys.argv) < 3:
 tag_name = sys.argv[1]
 release_body = sys.argv[2]
 
+
 # Split release_body by lines
 lines = release_body.splitlines()
 
@@ -25,17 +27,24 @@ if lines:
     lines = lines[1:]
 
 # Create a new header line
-header_line = f"## Patch Note - Version {tag_name} :apr:"
+header_line = f"## Patch Note - Version {tag_name} <:apr:1271743726225719296>"
 
 # Rebuild the body with our custom first line
 new_release_body = header_line + "\n" + "\n".join(lines)
+
+# Transform the last line to the desired format
+new_release_body = re.sub(
+    r'\*\*Full Changelog\*\*: https://github\.com/Azeroth-Pilot-Reloaded/azeroth-pilot-reloaded/compare/([\w\.]+...[\w\.]+)',
+    r'**Full Changelog**: [\1](https://github.com/Azeroth-Pilot-Reloaded/azeroth-pilot-reloaded/compare/\1)',
+    new_release_body
+)
 
 # Build the message to post on Discord
 message = new_release_body
 
 # Send the message to Discord
 print("Posting to Discord...")
-response = requests.post(DISCORD_WEBHOOK_URL, json={"content": message})
+response = requests.post(DISCORD_WEBHOOK_URL, json={"content": message, "flags": 4})
 if response.status_code != 204:
     print(f"Error posting to Discord: {response.status_code} {response.text}")
     sys.exit(1)
